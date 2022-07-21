ad09e67f32c2890ebbe74418ef50d8272663d6b5
sonora510 <103087155+sonora510@users.noreply.github.com>
2022-07-21 20:17:36 +0900

    FactoryBot(nickname修正)

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :user do
-    nickname { Faker::Name.initials(number: 2) }
+    nickname { Faker::JapaneseMedia::DragonBall.character }
     email {Faker::Internet.free_email}
     #password {Faker::Lorem.characters(min_alpha: 1, min_numric: 1 min_length: 6)}
     password {Faker::Internet.password(min_length: 6)}
