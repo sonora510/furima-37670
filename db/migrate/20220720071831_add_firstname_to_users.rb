40efb0caf349c063648c5bb1c88ddfa31343413b
sonora510 <103087155+sonora510@users.noreply.github.com>
2022-07-21 12:45:01 +0900

    usersテーブルカラム追加

--- db/migrate/20220720071831_add_firstname_to_users.rb
+++
@@ -1,5 +0,0 @@
-class AddFirstnameToUsers < ActiveRecord::Migration[6.0]
-  def change
-    add_column :users, :first_name_zen, :string
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,22 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_20_072032) do
+ActiveRecord::Schema.define(version: 2022_07_18_071852) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
+    t.string "nickname", null: false
+    t.string "family_name_zen", null: false
+    t.string "first_name_zen", null: false
+    t.string "family_name_kana", null: false
+    t.string "first_name_kana", null: false
+    t.date "birthday", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "nickname"
-    t.string "family_name_zen"
-    t.string "first_name_zen"
-    t.string "birthday"
-    t.string "family_name_kana"
-    t.string "first_name_kana"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
