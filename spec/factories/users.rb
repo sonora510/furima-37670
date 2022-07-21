e212bd9f757cc44eb04159935afafa79b7906ff0
sonora510 <103087155+sonora510@users.noreply.github.com>
2022-07-21 18:15:00 +0900

    FactoryBotの準備

---
+++ spec/factories/users.rb
@@ -0,0 +1,14 @@
+FactoryBot.define do
+  factory :user do
+    nickname { Faker::Name.initials(number: 2) }
+    email {Faker::Internet.free_email}
+    #password {Faker::Lorem.characters(min_alpha: 1, min_numric: 1 min_length: 6)}
+    password {Faker::Internet.password(min_length: 6)}
+    password_confirmation {password}
+    family_name_zen {"山田"}
+    first_name_zen {"太郎"}
+    family_name_kana {"やまだ"}
+    first_name_kana {"たろう"}
+    birthday {"1930-01-01"}
+  end
+end
