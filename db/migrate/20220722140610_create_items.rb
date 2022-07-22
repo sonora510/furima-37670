12a46282c7a0d69d32459f52a2b8e6e6359ebf5c
sonora510 <103087155+sonora510@users.noreply.github.com>
2022-07-22 23:20:13 +0900

    itemsテーブル作成

---
+++ db/migrate/20220722140610_create_items.rb
@@ -0,0 +1,17 @@
+class CreateItems < ActiveRecord::Migration[6.0]
+  def change
+    create_table :items do |t|
+      t.references :user,foreign_key: true
+      t.string :product, null: false
+      t.text :infomation, null: false
+      t.integer :prefecture_id, null: false
+      t.integer :category_id, null: false
+      t.integer :status_id, null: false
+      t.integer :delivery_cost_id, null: false
+      t.integer :delivery_day_id, null: false
+      t.integer :price, null: false
+
+      t.timestamps
+    end
+  end
+end
