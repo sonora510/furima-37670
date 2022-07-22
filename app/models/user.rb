2eded24e7e39f30c7a76c6fab8a5c1c7dbc016b8
sonora510 <103087155+sonora510@users.noreply.github.com>
2022-07-22 20:42:44 +0900

    不要項目のコメントアウト

--- app/models/user.rb
+++ app/models/user.rb
@@ -22,7 +22,7 @@ class User < ApplicationRecord
           validates :birthday
           validates :password, :password_confirmation, format: { with: /(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z0-9\d]{6,}/ }
          end
-          has_many :items
-          has_many :product_purchases
+          #has_many :items
+          #has_many :product_purchases
  end
  
