3159fb882266fa1a6426b4c64665dfcfc90b8eb1
sonora510 <103087155+sonora510@users.noreply.github.com>
2022-07-22 12:19:19 +0900

    FactoryBot パスを10字ランダム（英4、数1）のFakerへ修正

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,8 +2,11 @@ FactoryBot.define do
   factory :user do
     nickname { Faker::JapaneseMedia::DragonBall.character }
     email {Faker::Internet.free_email}
-    #password {Faker::Lorem.characters(min_alpha: 1, min_numric: 1 min_length: 6)}
-    password {Faker::Internet.password(min_length: 6)}
+    # password {Faker::Lorem.characters(min_alpha: 1, min_numric: 1, min_length: 6)}
+    password {Faker::Lorem.characters(number: 10, min_alpha: 4, min_numeric: 1)}
+    # password {Faker::Internet.password(min_length: 6)}
+    # password_confirmation {password}
+    #password {'test123'}
     password_confirmation {password}
     family_name_zen {"山田"}
     first_name_zen {"太郎"}
