0188cf5a98b96c0abda2cf91ddd79dbdfc49297d
sonora510 <103087155+sonora510@users.noreply.github.com>
2022-07-23 12:18:08 +0900

    ActiveHashモデルとのアソシエーション

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,23 @@
 class Item < ApplicationRecord
-  
+  extend ActiveHash::Associations::ActiveRecordExtensions
+
+  # ジャンルの選択時「--」の時は保存できない
+  with_options numerricality: { other_than: 1 } do
+    validates :category_id
+    validates :delivery_day_id
+    validates :delivery_cost_id
+    validates :prefecture_id
+    validates :status_id
+  end
+
+  # ActiveHash アソシエーション
+  belongs_to :category
+  belongs_to :delivery_day
+  belongs_to :delivery_cost
+  belongs_to :prefecture
+  belongs_to :status
+  belongs_to :user
+
   # 1:1 item:imageテーブル
   has_one_attached :image
 end
