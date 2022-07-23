897f7495ed16a53c449fbd60d77bf0b3c8a639ec
sonora510 <103087155+sonora510@users.noreply.github.com>
2022-07-23 21:29:57 +0900

    商品出品機能のモデルテスト実装

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,89 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '商品出品機能' do
+    before do
+      @item = FactoryBot.build(:item)
+    end
+
+  # 正常テスト
+  context '出品できる時' do
+    it '商品が出品できる' do
+      #binding.pry
+      expect(@item).to be_valid
+    end
+  end
+
+  # 異常テスト
+  context '出品に失敗するとき' do
+    it 'imageが空では登録できない' do
+      @item.image = nil
+      @item.valid?
+      #binding.pry
+      expect(@item.errors.full_messages).to include("Image can't be blank")
+    end
+    it '商品名が空では登録できない' do
+      @item.product = ''
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Product can't be blank")
+    end
+    it '商品説明が空では登録できない' do
+      @item.information = ''
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Information can't be blank")
+    end
+    it 'カテゴリーが空では登録できない' do
+      @item.category_id = 1
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Category must be other than 1")
+    end
+    it '商品状態が空では登録できない' do
+      @item.status_id = 1
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Status must be other than 1")
+    end
+    it '発送料の情報が空では登録できない' do
+      @item.delivery_cost_id = 1
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Delivery cost must be other than 1")
+    end
+    it '発送元の地域が空では登録できない' do
+      @item.prefecture_id = 1
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Prefecture must be other than 1")
+    end
+    it '発送までの日数が空では登録できない' do
+      @item.delivery_day_id = 1
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Delivery day must be other than 1")
+    end
+    it '価格が空では登録できない' do
+      @item.price = ''
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Price can't be blank")
+    end
+    it '価格は全角数字では登録できない' do
+      @item.price = '１２３４'
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Price is out of setting range")
+    end
+    it '価格は半角の英数字では登録できない' do
+      @item.price = '12ab'
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Price is out of setting range")
+    end
+    it '価格は¥300より低く設定できない' do
+      @item.price = '299'
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Price is out of setting range")
+    end
+    it '価格は¥999,999,999より高く設定できない' do
+      @item.price = '10000000'
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Price is out of setting range")
+    end
+  end
+  end
 end
+
+
