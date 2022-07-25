af450adc7b50f798aeaf74c6bb09a1e4ecb85252
sonora510 <103087155+sonora510@users.noreply.github.com>
2022-07-25 17:34:55 +0900

    バリデーション 記述ミス修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,7 +4,7 @@ class Item < ApplicationRecord
   with_options presence: true do
     validates :information
     validates :price,
-              numericality: { only_integer: ture, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999,
+              numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999,
                               message: 'is out of setting range' }
     validates :image
     validates :product
