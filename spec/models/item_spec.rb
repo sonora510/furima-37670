7c9049476291c4ba226ad94636be976b98bab1dc
sonora510 <103087155+sonora510@users.noreply.github.com>
2022-07-25 17:35:45 +0900

    商品出品テスト追加（Userの紐付き）

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -15,6 +15,12 @@ RSpec.describe Item, type: :model do
 
     # 異常テスト
     context '出品に失敗するとき' do
+      it 'userが紐づいていなければ出品できない' do
+        binding.pry
+        @item.user = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("User must exist")
+      end
       it 'imageが空では登録できない' do
         @item.image = nil
         @item.valid?
