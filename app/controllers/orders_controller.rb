1424a82d5ec0aedf813135a54f2d785f1c7df4e5
sonora510 <103087155+sonora510@users.noreply.github.com>
2022-08-03 14:42:54 +0900

    テーブルへの情報保存を記述

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,7 +2,7 @@ class OrdersController < ApplicationController
   # 非ログインユーザーをログインページへ誘導する
   before_action :authenticate_user!, only: [:index, :new, :create]
 
-  # 情報の引き渡し
+  # 商品情報の引き渡し
   before_action :set_item, only: [:index]
 
   def index
@@ -12,11 +12,25 @@ class OrdersController < ApplicationController
   end
 
   def create
+    @order =Order.create(order_params)
+    Addressbook.create(addressbook_params)
+    redirect_to root_path
   end
 
   private
 
+  # 商品情報
   def set_item
     @item = Item.find(params[:item_id])
+  end
+
+  # 購入情報
+  def order_params
+    params.permit(:item_id, :user_id).merge(user_id: current_user.id)
+  end
+
+  # 発送情報
+  def addressbook_params
+    params.permit(:order, :post_code, :prefecture_id, :city, :banti, :bilding_name, :phone_num).merge(order_id: @order.id)
   end
 end
