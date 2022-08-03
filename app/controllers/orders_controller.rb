df1eb33c5ed1165ae3046ed55d9650372c37f7c6
sonora510 <103087155+sonora510@users.noreply.github.com>
2022-08-03 19:50:36 +0900

    formオブジェクトの設定

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,19 +1,22 @@
 class OrdersController < ApplicationController
   # 非ログインユーザーをログインページへ誘導する
-  before_action :authenticate_user!, only: [:index, :new, :create]
+  before_action :authenticate_user!, only: [:index, :create]
 
   # 商品情報の引き渡し
   before_action :set_item, only: [:index]
 
   def index
-  end
-
-  def new
+    @order_addressbook = OrderAddressbook.new
   end
 
   def create
-    @order =Order.create(order_params)
-    Addressbook.create(addressbook_params)
+    @order_addressbook = OrderAddressbook.new(order_params)
+
+    if @order_addressbook.valid?
+      @order_addressbook.save
+    #@order =Order.create(order_params)
+    #Addressbook.create(addressbook_params)
+    end
     redirect_to root_path
   end
 
@@ -25,12 +28,15 @@ class OrdersController < ApplicationController
   end
 
   # 購入情報
+  # permit>フォームで入力する情報
   def order_params
-    params.permit(:item_id, :user_id).merge(user_id: current_user.id)
+    params.require(:order_addressbook)
+    .permit(:post_code, :prefecture_id, :city, :banti, :bilding_name,:phone_num)
+    .merge(item_id: params[:item_id], user_id: current_user.id)
   end
 
-  # 発送情報
-  def addressbook_params
-    params.permit(:order, :post_code, :prefecture_id, :city, :banti, :bilding_name, :phone_num).merge(order_id: @order.id)
-  end
+  # 発送情報ここは不要になる？
+  # def addressbook_params
+  # params.permit(:order, :post_code, :prefecture_id, :city, :banti, :bilding_name, :phone_num).merge(order_id: @order.id)
+  # end
 end
---
+++ app/models/order_addressbook.rb
@@ -0,0 +1,29 @@
+class OrderAddressbook
+  include ActiveModel::Model
+
+  # orderテーブル、addressbookテーブルへ保存するカラム
+  attr_accessor :user_id, :item_id, :order_id, :post_code, :prefecture_id, :city, :banti, :bilding_name, :phone_num
+
+  # バリデーション
+  with_options presence: true do
+    validates :user_id
+    validates :item_id
+  
+    validates :post_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: "Postnum can't be blank" }
+    validates :city
+    validates :banti
+    validates :phone_num, format: { with: /\A0(\d{1}?\d{4}|\d{2}?\d{3}|\d{3}?\d{2}|\d{4}?\d{1})?\d{4}\z|\A0[5789]0?\d{4}?\d{4}\z/ }
+  end
+  validates :prefecture_id, numericality: { other_than: 0, message: "Can't be blank"}
+
+  # データをテーブルへ保存
+  def save
+    # 購入情報を保存し、変数Orderへ代入 (カラム名: 保存したい値)
+    order = Order.create(item_id: item_id, user_id: user_id)
+    binding.pry
+    # 住所を保存
+    # order_idは変数orderのidと指定する
+    Addressbook.create(order_id: order.id, post_code: post_code, prefecture_id: prefecture_id, city: city, banti: banti,bilding_name: bilding_name, phone_num: phone_num)
+
+    end
+end
