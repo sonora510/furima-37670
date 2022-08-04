af2634764efa898273014c5037a6fc30b933ffb6
sonora510 <103087155+sonora510@users.noreply.github.com>
2022-08-04 21:10:01 +0900

    不要項目の削除

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
    get 'items/new', to: 'items#new'
 
   resources :items do
-    resources :orders, only: [:index, :nuw, :create]
+    resources :orders, only: [:index, :create]
   end
 end
